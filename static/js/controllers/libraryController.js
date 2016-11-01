ab81c84df21b0bfacf62129fdcb5e0b7665745a1
gambastian <sebas.gamba02@gmail.com>
2016-11-01 00:10:12 -0500

    formulario para ver las colleciones de un artista

---
+++ static/js/controllers/libraryController.js
@@ -0,0 +1,12 @@
+/**
+ * Created by TOSHIBA on 26/09/2016.
+ */
+(function () {
+    'use strict';
+
+    var LibraryCrtl = function ($rootScope, $scope, $location, $routeParams, $route) {
+
+    };
+
+    angular.module('freesounds.controllers').controller('LibraryCrtl', ['$rootScope', '$scope', '$location', '$routeParams', '$route', LibraryCrtl]);
+}());
