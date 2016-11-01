638a477d55645f06a91f6ed1529f6a070008567d
gambastian <sebas.gamba02@gmail.com>
2016-10-31 19:04:57 -0500

    Ajuste para ejecutar cancion desde la lista de piezas

--- static/js/controllers/piecesController.js
+++ static/js/controllers/piecesController.js
@@ -6,16 +6,30 @@
 
     var PiecesCrtl = function ($rootScope, $scope, $location, piecesService) {
 
+        $rootScope.songs = [];
+
         var res = piecesService.list().then(function (data) {
-            $scope.pieces = data;
+            for (var i = 0; i < data.length; i++) {
+
+                var tempSong = {
+                    id: data[i].pk,
+                    title: data[i].fields.name,
+                    artist: data[i].fields.artist.name,
+                    url: data[i].fields.url,
+                    duration: data[i].fields.duration,
+                    image_cover: data[i].fields.image_cover
+                };
+
+                $rootScope.songs.push(tempSong);
+            }
         }, function (response) {
             $scope.error = true;
             console.log('Error: ' + response);
-        })
+        });
 
         $scope.viewDetail = function (piece_id) {
             $location.url('/pieces/' + piece_id);
-        }
+        };
 
     };
 
