73a8b857435a09dbe48cfdf332639d470851b683
gambastian <sebas.gamba02@gmail.com>
2016-11-01 19:42:48 -0500

    Logica de creacion de listas desde el form

--- static/js/controllers/libraryController.js
+++ static/js/controllers/libraryController.js
@@ -6,6 +6,20 @@
 
     var LibraryCrtl = function ($rootScope, $scope, $location, $routeParams, $route) {
 
+        $scope.isNewCollection = true;
+        $scope.newCollection = {};
+        $scope.collections = [];
+
+        $scope.toggleIsNewCollection = function () {
+            $scope.isNewCollection = !$scope.isNewCollection;
+            $scope.newCollection = {};
+        };
+
+        $scope.saveCollection = function () {
+            console.log($scope.newCollection);
+            $scope.collections.push($scope.newCollection);
+            $scope.isNewCollection = true;
+        };
     };
 
     angular.module('freesounds.controllers').controller('LibraryCrtl', ['$rootScope', '$scope', '$location', '$routeParams', '$route', LibraryCrtl]);
